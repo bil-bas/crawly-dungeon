8d5507ec2f5d66c064c1b010a3a3428caaae9ecc
Bil Bas <bil.bagpuss@gmail.com>
2025-07-31 20:19:50 +0100

    Update pxt.json, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "key":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . 5 5 5 5 5 5 . . . . 
+. . . . . 5 5 5 5 5 3 3 . . . . 
+. . . 5 5 5 . . . . 5 3 . . . . 
+. . . 5 . . . . . . . 5 3 . . . 
+. . . 5 . . . . . . . 5 3 . . . 
+. . . 5 . . . . . . . 5 . . . . 
+. . . 5 . . . . . 5 5 3 . . . . 
+. . . . 5 5 5 5 5 5 3 . . . . . 
+. . . . . 3 3 5 3 3 . . . . . . 
+. . . . . . . 5 . . . . . . . . 
+. . . . . . . 5 5 5 . . . . . . 
+. . . . . . . 5 5 5 5 . . . . . 
+. . . . . . . 5 . . . . . . . . 
+`;
+            case "image3":
+            case "witchForward0":return img`
+. . . . . . . c c c . . . . . . 
+. . . . . . c b 5 c . . . . . . 
+. . . . c c c 5 5 c c c . . . . 
+. . c c b c 5 5 5 5 c c c c . . 
+. c b b 5 b 5 5 5 5 b 5 b b c . 
+. c b 5 5 b b 5 5 b b 5 5 b c . 
+. . f 5 5 5 b b b b 5 5 5 c . . 
+. . f f 5 5 5 5 5 5 5 5 f f . . 
+. . f f f b f e e f b f f f . . 
+. . f f f 1 f b b f 1 f f f . . 
+. . . f f b b b b b b f f . . . 
+. . . e e f e e e e f e e . . . 
+. . e b c b 5 b b 5 b f b e . . 
+. . e e f 5 5 5 5 5 5 f e e . . 
+. . . . c b 5 5 5 5 b c . . . . 
+. . . . . f f f f f f . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
  
-let levels = [
+const levels = [
     tilemap`level 1`,
     tilemap`level 2`,
     tilemap`level 3`
 ]
 
-tiles.setCurrentTilemap(levels[0])
+tiles.setCurrentTilemap(levels[1])
+
+let mySprite = sprites.create(assets.image`witchForward0`, SpriteKind.Player)
+controller.moveSprite(mySprite, 50, 50)
